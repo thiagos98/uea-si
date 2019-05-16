0f475932b83fb8f1286ed738fa05075cb80880b8
Thiago Santos Borges <thiagosantosborges18@gmail.com>
2019-05-16 06:15:58 -0400

    teste

--- semp-tcl/AULAS-SEMP/sprint01/dados-pessoais.php
+++ semp-tcl/AULAS-SEMP/sprint01/dados-pessoais.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,23 +8,22 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<!--<script src="js/script.js"></script>-->
 </head>
 <body class="text-center" id="body">
 	<div id="border-external">
 		<h1 class="h3">DADOS PESSOAIS</h1>
 		<img src="images/image1.jpg" class="rounded-circle img-thumbnail" alt="Foto 3x4 da pessoa">
-		<div>
-			<h1 class="h5" id="nome"></h1>
-			<h1 class="h5" id="curso">CURSO</h1>
-			<h1 class="h5" id="universidade">UNIVERSIDADE</h1>
+		<h1 class="h5" id="nome"><?php echo $_POST["nome"]; ?></h1>
+		<h1 class="h5" id="curso"><?php echo $_POST["curso"]; ?></h1>
+		<h1 class="h5" id="universidade"><?php echo $_POST["universidade"]; ?></h1>
+		<div class="container">
+			<h1 class="h6">RESUMO PROFISSIONAL:</h1>
+			<p class="texto" id="profissional"><?php echo $_POST["profissional"]; ?></p>
 		</div>
 		<div class="container">
-			<h1 class="h6">RESUMO PROFISSIONAL</h1>
-			<p class="text" id="professional">BLABLABLABLALBLASDASDLKSAKDJHSKJFHSAKDHFKASDHFAGSDFJHGASDJHFGASJKDHFGJASDHGFdasdasdadsdassdaadssdadsaJASH</p>
-		</div>
-		<div class="container">
-			<h1 class="h6">RESUMO ACADÊMICO</h1>
-			<p class="text" id="academico">BLABLABLABLALBLASDASDLKSAKDJHSKJFHSAKDHFKASDHFAGSDFJHGASDJHFGASJKDHFGJASDHGFJASH</p>
+			<h1 class="h6">RESUMO ACADÊMICO:</h1>
+			<p class="texto" id="academico"><?php echo $_POST["academico"]; ?></p>
 		</div>
 	</div>
 </body>
