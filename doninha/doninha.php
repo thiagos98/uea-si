252214012e1a2e0d44d84fdb6c0df6c950f546be
Thiago Santos Borges <thiagosantosborges18@gmail.com>
2019-05-19 20:28:36 -0400

    comeco doninha

---
+++ doninha/doninha.php
@@ -0,0 +1,68 @@
+<?php
+    const CONST_SIZE = 100;
+    const CONST_PROB = 5;
+
+    $target = $_POST["target"];
+    $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    function randomChr() {
+        $alphabet = $GLOBALS['alphabet'];
+        return $alphabet[rand(0,26)];
+    }
+
+    function randomStr() {
+        $target = $GLOBALS['target'];
+        $randomStr = '';
+        for ($i=0; $i < strlen($target); $i++) {
+            $randomStr = $randomStr . randomChr();
+        }
+        return $randomStr;
+    }
+
+    function compareStr($randomStr){
+        $target = $GLOBALS['target'];
+        $randomStrIndex = 0;
+        for ($i=0; $i < strlen($target); $i++) {
+            if($target[$i] == $randomStr[$i]){
+                $randomStrIndex++;
+            }
+        }
+        return $randomStrIndex;
+    }
+
+    function modRandomStr($randomStr){
+        $target = $GLOBALS['target'];
+        $newRandomStr = $randomStr;
+        for ($i=0; $i < strlen($target); $i++) {
+            $randomInt = rand(1,100);
+            if($randomInt <= CONST_PROB){
+                $newRandomStr = substr($newRandomStr, 0, $i) . randomChr() . substr($newRandomStr, ($i + 1));
+            }
+        }
+        return $newRandomStr;
+    }
+
+    $loopNumber = 0;
+    $strFound = FALSE;
+    $randStr = randomStr();
+    $resultList = [];
+    while(!$strFound){
+        for ($i=0; $i < CONST_SIZE; $i++) {
+            // code...
+            $strList[$i] = '';
+            $indexList[$i] = NULL;
+        }
+        for ($i=0; $i < CONST_SIZE; $i++) {
+            $strList[$i] = modRandomStr($randStr);
+            $indexList[$i] = compareStr($strList[$i]);
+        }
+        $maxIndex = max($indexList);
+        if($maxIndex == strlen($target)){
+            $strFound = TRUE;
+        }
+        $index = $indexList[$maxIndex];
+        $randStr = $strList[$index];
+        echo($loopNumber . ": " . $randStr . " -- Score: ". $maxIndex . "<br>");
+        $loopNumber++;
+    }
+ ?>
